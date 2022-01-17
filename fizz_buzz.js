373a9e0ee06365cb561a42209e8422a4105a5d5c
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-01-17 15:51:18 +0000

    no if else

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,6 +1,10 @@
 for (let i=1; i<=100; i++) {
-    if (i%5===0 && i%3===0) { console.log("FizzBuzz");}
-    else if (i%3===0) { console.log("Fizz");}
-    else if (i%5===0) { console.log("Buzz");}
-    else {console.log(i);}   
+    // if (i%5===0 && i%3===0) { console.log("FizzBuzz");}
+    // else if (i%3===0) { console.log("Fizz");}
+    // else if (i%5===0) { console.log("Buzz");}
+    // else {console.log(i);}   
+
+
+    var s = (i%5===0 && i%3===0) ? 'FizzBuz' : (i%3===0) ? "Fizz": (i%5===0) ? "Buzz" : i.toString();
+    console.log(s);
 }
