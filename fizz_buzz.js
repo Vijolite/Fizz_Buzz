e64a4d762d833859f7b3fbafc8ab947579b8e6c7
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-01-18 11:46:58 +0000

    spelling

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -18,7 +18,7 @@ for (let i=1; i<=100; i++) {
     //var s = isDivisibleBy(i,5) && isDivisibleBy(i,3) ? 'FizzBuzz' : isDivisibleBy(i,3) ? "Fizz": isDivisibleBy(i,5) ? "Buzz" : i.toString();
     //console.log(s);
 
-    //4rt version
+    //4th version
     let result='';
     if (isDivisibleBy(i,3)) result+="Fizz";
     if (isDivisibleBy(i,5)) result+="Buzz";
